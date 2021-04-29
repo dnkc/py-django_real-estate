167bc9996eaaa30ec487f37a7b12def9c34d0460
dnkc <dnkcdesign@gmail.com>
2021-04-28 21:44:42 -0400

    added realtor & listing models

--- dkre/dkre/settings.py
+++ dkre/dkre/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))yy
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'dkre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dkre',
+        'USER': 'postgres',
+        'PASSWORD': '1dk2post3',
+        'HOST': 'localhost',
     }
 }
 
@@ -125,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dkre/static')
 ]
+
+# media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- dkre/listings/admin.py
+++ dkre/listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# where you customize admin stuff for listings app
+from .models import Listing
+
+admin.site.register(Listing)
