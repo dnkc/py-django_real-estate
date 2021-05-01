b11f6cedbe90aed8cdd53a06e020bd4a7d106fd5
dnkc <dnkcdesign@gmail.com>
2021-05-01 16:17:35 -0400

    added listings & search function

--- dkre/listings/admin.py
+++ dkre/listings/admin.py
@@ -5,4 +5,12 @@ from django.contrib import admin
 # where you customize admin stuff for listings app
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', )
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
--- dkre/listings/views.py
+++ dkre/listings/views.py
@@ -1,12 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .choices import price_choices, bedroom_choices, state_choices
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True) #fetches listings from the database
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings,
+
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords and len(keywords) > 0:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+    # city filter
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city and len(city) > 0:
+            queryset_list = queryset_list.filter(city__iexact=city) # iexact is case INsensitive
+    # province
+    if 'province' in request.GET:
+        province = request.GET['province']
+        if province and len(province) > 0:
+            queryset_list = queryset_list.filter(province__iexact=province)
+    # bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms and len(bedrooms) > 0:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms).order_by('-bedrooms')
+    # price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price and len(price) > 0:
+            queryset_list = queryset_list.filter(price__lte=price).order_by('-price')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
